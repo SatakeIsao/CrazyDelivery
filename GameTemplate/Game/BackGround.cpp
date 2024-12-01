9454707ae3625e0fffac43eb5923a6e6757f87a0
SatakeIsao <ca01234009@kawahara.ac.jp>
2024-12-02 00:15:55 +0900

    遮蔽物という属性を付与、ステージの座標を原点に設置、テストでフォントのテキストを追加

--- GameTemplate/Game/BackGround.cpp
+++ GameTemplate/Game/BackGround.cpp
@@ -14,23 +14,37 @@ BackGround::~BackGround()
 bool BackGround::Start()
 {
 	Init();
-
+	//FontInitTest();
 	return true;
 }
 
 void BackGround::Init()
 {
-	m_position.Set(1000.0f,0.0f,15500.0f);
-	m_bgModel.Init("Assets/stageData/map/map7.tkm",0,0,enModelUpAxisY,false);
-	//m_bgModel.Init("Assets/modelData/bg.tkm", 0, 0, enModelUpAxisY, false);
+	m_position.Set(0.0f,0.0f,0.0f);
+	m_bgModel.Init("Assets/stageData/map/map8.tkm",0,0,enModelUpAxisY,false);
 	m_bgModel.SetPosition(m_position);
 	m_bgModel.SetScale(0.6f);
 	m_bgModel.Update();
 
 	//静的物理オブジェクトを作成
 	physicsStaticObject.CreateFromModel(m_bgModel.GetModel(), m_bgModel.GetModel().GetWorldMatrix());
+
+	//遮蔽物という属性を付与する
+	physicsStaticObject.GetbtCollisionObject()->setUserIndex(enCollisionAttr_Wall);
 }
 
+void BackGround::FontInitTest()
+{
+	m_fontTest2.SetText(L"testnow");
+	m_fontTest2.SetScale(1.0f);
+	m_fontTest2.SetPosition(Vector3(0.0f, 0.0f, 0.0f));
+	m_fontTest2.SetColor({ 1.0f,1.0f,1.0f,1.0f });
+}
+
+//const PhysicsStaticObject& BackGround::GetPhysicsObject() const
+//{
+//	return physicsStaticObject;
+//}
 
 void BackGround::Update()
 {
@@ -40,4 +54,5 @@ void BackGround::Update()
 void BackGround::Render(RenderContext& rc)
 {
 	m_bgModel.Draw(rc);
+	//m_fontTest2.Draw(rc);
 }
--- GameTemplate/Game/BackGround.h
+++ GameTemplate/Game/BackGround.h
@@ -7,13 +7,15 @@ public:
 	~BackGround();
 	bool Start();
 	void Init();
-
+	void FontInitTest();
 	void Update();
 	void Render(RenderContext& rc);
 
+	//const PhysicsStaticObject& GetPhysicsObject() const;
 	
 private:
 	ModelRender m_bgModel;	//背景モデル
+	FontRender m_fontTest2;
 	PhysicsStaticObject m_phyStaticObject;	//静的物理オブジェクト
 	Vector3 m_position = Vector3::Zero;		//座標
 	Vector3 m_scale = Vector3::One;			//大きさ
