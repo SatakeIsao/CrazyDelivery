c8322c0c4723e431ac22d7091a83a824424f0fbb
SatakeIsao <ca01234009@kawahara.ac.jp>
2024-12-02 09:06:57 +0900

    プレイヤーと壁が当たると反射で跳ね返る。数値を調べるための文字出力関数を追加。アニメーション再生のスピードを2倍にした。

--- GameTemplate/Game/Player.cpp
+++ GameTemplate/Game/Player.cpp
@@ -5,6 +5,26 @@
 #include "PlayerStartState.h"
 #include "PlayerPushState.h"
 #include "GameCamera.h"
+#include "BackGround.h"
+#include <fstream>
+#include <iostream>
+
+namespace
+{
+	//const float MAX_ACCELERATION = 500.0f;			//最大加速度
+	//const float MAX_DECELERATION = 0.2f;				//最大減衰度
+	
+	const float CHARACON_RADIUS = 20.0f;				//キャラクターコントローラーの半径
+	const float CHARACON_HEIGHT = 50.0f;				//キャラクターコントローラーの高さ
+	const float DECELERATION_TIME = 5.0f;				//減速にかかる時間(秒)
+	const float STOP_THRESHOLD = 0.1f;					//速度停止のしきい値
+	const float ROT_SPEED = -0.01f;						//回転速度(仮の値、適宜調整)
+	const float DRIFT_ROT_SPEED_MULTPLAER = 5.0f;		//ドリフト中の回転速度倍率
+	const float GRAVITY = 15.0f;						//重力加速度(仮の値、適宜調整)
+	const float GROUND_LEVEL = 0.0f;					//地面の高さ
+	const float COLLISION_UP_OFFSET = CHARACON_HEIGHT;	//コリジョンチェックのY軸オフセット
+	const float SPEED_THRESHOLD = 1.5f;					//スピードリセットのしきい値
+}
 
 namespace nsPlayer
 {
@@ -18,7 +38,8 @@ namespace nsPlayer
 
 	bool Player::Start()
 	{
-		//アニメーションクリップを初期化
+		m_backGround = FindGO<BackGround>("background");
+		//プレイヤーのアニメーションクリップを初期化
 		InitPlayerAnimationClips();
 
 		//プレイヤーモデルの初期化
@@ -38,26 +59,82 @@ namespace nsPlayer
 
 		//キャラクターコントローラーを初期化
 		m_charaCon.Init(
-			20.0f,		//半径
-			120.0f,		//高さ
-			m_position	//座標
+			CHARACON_RADIUS,		//半径
+			CHARACON_HEIGHT,		//高さ
+			m_position	//初期位置設定
 		);
 
-		InitQuietTimeSet();	//減速度設定
+		//衝突確認用のコライダーを初期化
+		//半径はキャラクターコントローラーと同じ
+		//高さはキャラクターコントローラーの2分の1
+		m_capsuleCollider.Init(CHARACON_RADIUS, CHARACON_HEIGHT / 2);
 
-		//初期ステートを設定
+		PlaySetAnimationSpeed(2.0f);
+
+		//減速度の初期設定
+		InitQuietTimeSet();
+
+		//初期ステートを待機状態に設定
 		m_playerState = new PlayerIdleState(this);
-		m_playerState->Enter();
+		m_playerState->Enter();		//待機状態の初期処理
+		m_forward = Vector3::Right;
 		return true;
+	}
+
+	void Player::Update()
+	{
+		//ステートの変更のチェックと変更
+		IPlayerState* playerState = m_playerState->StateChange();
+
+		//新しいステートが設定されている場合、ステートを変更
+		if (playerState != nullptr)
+		{
+			delete m_playerState;		//現在のステートを削除
+			m_playerState = playerState;//新しいステートに変更
+			m_playerState->Enter();		//新しいステートの初期処理
+		}
+		//プレイヤーの移動処理
+		Move();
+		//移動ベクトルをチェックしてスピードをリセット
+		//CheckSpeedFromMovement();
+
+		
+
+		//減速処理
+		Friction();
+		//テキストファイルに現在のパラメータを書き込む
+		//Output();
+
+		//Jump();
+		//MoveLStickOn();
+		//tsts.AddRotationDegZ(g_pad[0]->GetLStickXF());
+		//tsts.AddRotationDegY(g_pad[0]->GetLStickXF());
+
+
+		//現在のステートの更新
+		m_playerState->Update();
+		
+		//アニメーションを再生する
+		PlayAnimation(m_currentAnimationClip);
+		//壁との衝突チェック
+		CheckCollisionWithWall();
+		//モデルの回転を更新
+		m_playerModel.SetRotation(m_rotation);
+		m_boardModel.SetRotation(m_rotation);
+
+		//モデルの更新
+		m_playerModel.Update();
+		m_boardModel.Update();
 	}
 
 	void Player::Speed()
 	{
+		//カメラの前方向を取得し、Y軸を無視した上で正規化
 		Vector3 forwardDirection = g_camera3D->GetForward();
 		forwardDirection.y = 0.0f;
 		forwardDirection.Normalize();
-		//現在の速度方向と加速しようとしている方向を比較
 
+		//現在の速度ベクトルを正規化し、進行方向との内積を計算して比較
 		Vector3 normalizedVelocity = m_velocity;
 		normalizedVelocity.Normalize();
 		float dotProduct = normalizedVelocity.Dot(forwardDirection);
@@ -66,25 +143,188 @@ namespace nsPlayer
 
 	void Player::Friction()
 	{
-		// 減速時間を5秒で設定
+		// 減速にかかる時間を5秒で設定
 		float decelerationTime = 5.0f;
 		float frameDeltaTime = g_gameTime->GetFrameDeltaTime();
 
 		// 減衰係数を計算（1フレームあたりに速度をどの割合で減らすか）
 		float decelerationFactor = 1.0f - (frameDeltaTime / decelerationTime);
 
-		// 減衰係数が範囲を超えないように制限
+		// 減衰係数が負の数にならないように制限
 		if (decelerationFactor < 0.0f) {
 			decelerationFactor = 0.0f;
 		}
+		// ボードの方向と速度の方向で摩擦力を変化させる
+		// -> ボードが横を向いていたら摩擦力が強くなるようにする
+		auto velDir = m_velocity;
+		velDir.Normalize();
+		auto t = pow(max(0.0f, velDir.Dot(m_forward)), 0.9f);
+		decelerationFactor *= Math::Lerp(t,  0.998f, 1.0f); // ボードが真横を向いている場合は4ばいの摩擦力
 
-		// 速度を減衰係数で減少させる
+		// 速度ベクトルに減衰係数を適用して減速
 		m_velocity *= decelerationFactor;
-		// もし速度ベクトルの長さが0.1未満であれば
-		if (m_velocity.Length() < 0.1f) {
-			m_velocity = Vector3::Zero; // 速度を(0, 0, 0)にリセット
+
+		// 速度ベクトルの長さが0.1未満であれば、完全に停止（速度を0にリセット）
+		if (m_velocity.Length() < STOP_THRESHOLD) {
+			m_velocity = Vector3::Zero;
 		}
 	}
+
+	void Player::Output()
+	{
+		//プレイヤーのデータをテキストスキャンに出力
+		std::ofstream outFile("PlayerData.txt", std::ios::app);
+		if (outFile.is_open())
+		{
+			outFile << "m_velocity:" << m_velocity.x << "  /  " << m_velocity.y << "  /  " << m_velocity.z << "\n";
+			outFile << "m_velocity.Length():" << m_velocity.Length() << "\n";
+			outFile << "m_forward.Length():" << m_forward.Length() << "\n";
+			//outFile << "m_accele:" << m_accele.x << "  /  " << m_accele.y << "  /  " << m_accele .z << "\n";
+			//outFile << "initQuietSpeed:" << initQuietSeppd << "\n";
+			//outFile << "m_position" << m_position.x << "  /  " << m_position.y << "  /  " << m_position.z << "\n";
+			outFile << "*------------------------------*\n";
+
+			outFile.close();
+		}
+		else
+		{
+			std::cerr << "Falled to open PlayerData.txt for writing." << std::endl;
+		}
+	}
+
+	void Player::BackGroundCollisionCheck()
+	{
+		//背景のコリジョンの配列を取得する
+		const auto& backGroundCollision = g_collisionObjectManager->FindCollisionObjects("player");
+		//コリジョンの配列をfor文で回す
+		for (auto collision : backGroundCollision){
+			//コリジョンとキャラコンが衝突したら
+			if (collision->IsHit(m_charaCon))
+			{
+				m_velocity = Vector3::Zero;
+				//return true;
+			}
+		}
+	}
+	struct SweepResultWall :public btCollisionWorld::ConvexResultCallback
+	{
+		//衝突フラグ
+		bool isHit = false;
+		Vector3 normal;
+		virtual btScalar addSingleResult(btCollisionWorld::LocalConvexResult& convexResult, bool normalInWorldSpace)
+		{
+			//壁とぶつかっていなかったら
+			if (convexResult.m_hitCollisionObject->getUserIndex() != enCollisionAttr_Wall) {
+				//衝突したのは壁ではない
+				return 0.0f;
+			}
+
+			//壁とぶつかったらフラグをtrueに
+			isHit = true;
+			normal.x = convexResult.m_hitNormalLocal.x();
+			normal.y = convexResult.m_hitNormalLocal.y();
+			normal.z = convexResult.m_hitNormalLocal.z();
+			return 0.0f;
+		}
+	};
+
+	void Player::CheckSpeedFromMovement()
+	{
+		nextPosition = m_charaCon.Execute(m_velocity, g_gameTime->GetFrameDeltaTime());
+		m_charaCon.SetPosition(m_position);
+
+		//次のフレームの座標を計算
+		//Vector3 nextPosition = m_position + (m_velocity * g_gameTime->GetFrameDeltaTime());
+		
+		//現在の座標と次のフレームの座標の移動ベクトルを計算
+		movementVector = nextPosition - m_position;
+		//移動ベクトルの長さを計算
+		movementLength = movementVector.Length();
+
+		//移動ベクトルが、しきい値以下ならスピードをリセット
+		//const float threshold = 1.5f;
+		if (movementLength < SPEED_THRESHOLD)
+		{
+			m_velocity = Vector3::Zero;
+			return;
+		}
+		
+		//背景コリジョンとプレイヤーの次フレーム座標の近接をチェック
+		//const PhysicsStaticObject& bgPhysics = m_backGround
+		//if(m_backGround)
+
+	}
+
+	
+	void Player::CheckCollisionWithWall()
+	{
+		//コライダーの始点と終点
+		btTransform start, end;
+		start.setIdentity();
+		end.setIdentity();
+
+		//始点はPlayerの現在の座標
+		//Yがm_positionのままだとコライダー地面と干渉するのでYを上げる
+		float up = CHARACON_HEIGHT;
+		start.setOrigin(btVector3(m_position.x, m_position.y + up, m_position.z));
+
+		
+		//終点はプレイヤーの前方向に進んだ位置
+		//進む距離を求める
+		Vector3 move = m_velocity * g_gameTime->GetFrameDeltaTime();
+		//進んだ先を求める
+		end.setOrigin(btVector3(m_position.x + move.x, m_position.y + move.y+ up,  m_position.z + move.z));
+		
+		
+		char text[256];
+		sprintf(text, "Ppos = %d,%d,%d  vel = %d,%d,%d\n", int(m_position.x), int(m_position.y), int(m_position.z), int(move.x), int(move.y), int(move.z));
+		OutputDebugStringA(text);
+
+		//スタートとエンドの座標が同じ場合通すとエラーを吐かれるので処理を終了する
+		if (start.getOrigin() == end.getOrigin())
+		{
+			return;
+		}
+
+		SweepResultWall callback;
+		
+		//PhysicsWorld::GetInstance()->ConvexSweepTest((const btConcaveShape*)m_sphereCollider.GetBody(), start, end, callback);
+		PhysicsWorld::GetInstance()->ConvexSweepTest((const btConvexShape*)m_capsuleCollider.GetBody(), start, end, callback);
+		
+
+		//壁と衝突したら
+		if (callback.isHit == true)
+		{
+			// callback.normal（N）とm_velocity（F）を使って反射ベクトルを求める
+			Vector3 normalXZ = callback.normal;
+			normalXZ.y = 0.0f;
+			normalXZ.Normalize();
+			Vector3 velDir = m_velocity;
+			velDir.Normalize();
+			m_reflection = normalXZ * m_velocity.Dot(normalXZ * -2.0f);
+
+			//m_velocity = m_reflection;
+			m_reflection += m_velocity;
+			// 進入角度によって減速率を計算する
+			float t = max(0, velDir.Dot(normalXZ) * -1.0f);
+
+			m_velocity = m_reflection * Math::Lerp(t, 0.7f, 0.2f);
+			//m_velocity = Vector3::Zero;
+			
+		}
+	}
+
+	void Player::ApplySpeedLimit()
+	{
+		currentSpeed = m_velocity.Length();		  //現在の速度を計算
+		if (currentSpeed > SpeedLimit) {
+			m_velocity.Normalize();				  //速度ベクトルを正規化
+			m_velocity *= SpeedLimit;			  //上限速度を適用
+		}
+	}
+
+
+
 
 	void Player::InitPlayerAnimationClips()
 	{
@@ -93,21 +333,25 @@ namespace nsPlayer
 		m_playerAnimClips[enAnimClip_Idle].SetLoopFlag(true);
 		m_boardAnimClips[enAnimClip_Idle].Load("Assets/animData/board/idle.tka");
 		m_boardAnimClips[enAnimClip_Idle].SetLoopFlag(true);
+
 		//スタートランのアニメーション
 		m_playerAnimClips[enAnimClip_Start].Load("Assets/animData/skater/start.tka");
 		m_playerAnimClips[enAnimClip_Start].SetLoopFlag(false);
 		m_boardAnimClips[enAnimClip_Start].Load("Assets/animData/board/start.tka");
 		m_boardAnimClips[enAnimClip_Start].SetLoopFlag(false);
+
 		//地面を蹴る時のアニメーション
 		m_playerAnimClips[enAnimClip_Push].Load("Assets/animData/skater/push.tka");
 		m_playerAnimClips[enAnimClip_Push].SetLoopFlag(false);
 		m_boardAnimClips[enAnimClip_Push].Load("Assets/animData/board/push2.tka");
 		m_boardAnimClips[enAnimClip_Push].SetLoopFlag(false);
+
 		//走っている時のアニメーション
 		m_playerAnimClips[enAnimClip_Run].Load("Assets/animData/skater/run.tka");
 		m_playerAnimClips[enAnimClip_Run].SetLoopFlag(false);
 		m_boardAnimClips[enAnimClip_Run].Load("Assets/animData/board/push2.tka");
 		m_boardAnimClips[enAnimClip_Run].SetLoopFlag(false);
+
 		//ジャンプする時のアニメーション
 		m_playerAnimClips[enAnimClip_Jump].Load("Assets/animData/skater/jumpFast.tka");
 		m_playerAnimClips[enAnimClip_Jump].SetLoopFlag(false);
@@ -122,74 +366,53 @@ namespace nsPlayer
 	}
 
 
-	void Player::Update()
-	{
-		//ステートを変更するか
-		IPlayerState* playerState = m_playerState->StateChange();
-		//変更先のステートが設定されているならば
-		if (playerState != nullptr)
-		{
-			//ステートを変更する
-			delete m_playerState;
-			m_playerState = playerState;
-			m_playerState->Enter();
-		}
-
-		Move();
-		Friction();
-		//Jump();
-		//MoveLStickOn();
-		//tsts.AddRotationDegZ(g_pad[0]->GetLStickXF());
-		//tsts.AddRotationDegY(g_pad[0]->GetLStickXF());
-
-		//各ステートの更新処理を実行
-		m_playerState->Update();
-		//アニメーションを再生する
-		PlayAnimation(m_currentAnimationClip);
-		//モデルを更新
-		m_playerModel.SetRotation(m_rotation);
-		m_boardModel.SetRotation(m_rotation);
-		//m_playerModel.SetRotation(tsts);
-		m_playerModel.Update();
-		m_boardModel.Update();
-	}
+	
 
 	void Player::PlayAnimation(EnAnimationClip currentAnimtionClip)
 	{
-		//アニメーションを再生
+		//指定されたアニメーションクリップを再生
 		m_playerModel.PlayAnimation(currentAnimtionClip, m_complementTime);
 		m_boardModel.PlayAnimation(currentAnimtionClip, m_complementTime);
 	}
 
+	void Player::PlaySetAnimationSpeed(float m_animationSpeed)
+	{
+		m_playerModel.SetAnimationSpeed(m_animationSpeed);
+		m_boardModel.SetAnimationSpeed(m_animationSpeed);
+	}
+
+
 
 	void Player::Move()
 	{
-		
-		float lStick_x = g_pad[0]->GetLStickXF() * RotSpeed;
-		//float lStick_y = g_pad[0]->GetLStickYF();
+		//左スティックのｘ軸入力を取得し、回転速度を適用
+		float lStick_x = g_pad[0]->GetLStickXF() * ROT_SPEED;
 
+		//ドリフト中の場合、回転速度を増加
 		if (m_isDrifting == true)
 		{
-			lStick_x = g_pad[0]->GetLStickXF() * RotSpeed * 5.0f;
+			lStick_x = g_pad[0]->GetLStickXF() * ROT_SPEED * DRIFT_ROT_SPEED_MULTPLAER;
 		}
+		//もとの回転速度に戻す
 		else
 		{
-			lStick_x = g_pad[0]->GetLStickXF() * RotSpeed;
+			lStick_x = g_pad[0]->GetLStickXF() * ROT_SPEED;
 		}
-		m_forward.x = m_forward.x * cos(lStick_x ) - m_forward.z * sin(lStick_x);
+		//プレイヤーの進行方向ベクトルの回転
+		m_forward.x = m_forward.x * cos(lStick_x) - m_forward.z * sin(lStick_x);
 		m_forward.z = m_forward.x * sin(lStick_x) + m_forward.z * cos(lStick_x);
+		m_forward.Normalize();
 		m_rotation.SetRotationY(atan2(m_forward.x, m_forward.z));
 		//if (lStick_x >= 0.0f)
 		
-
-	
-
+		//カメラの前方向を基準に速度を計算
 		Vector3 cameraForward = g_camera3D->GetForward();
 		cameraForward.y = 0.0f;
 		cameraForward.Normalize();
+
 		// 現在の速度を取得して上限速度との関係から加速度の割合を計算
-		float currentSpeed = m_velocity.Length();
-		float speedLimit = SpeedLimit;
+		//float currentSpeed = m_velocity.Length();
+		//float speedLimit = SpeedLimit;
 
 		//加速時間が 0秒より大きい時、加速時間のカウントダウンを行う
 		if (m_acceleDelayTime > 0.0f) {
@@ -198,12 +421,19 @@ namespace nsPlayer
 			//加速時間が負の値になった時、0秒にする
 			if (m_acceleDelayTime < 0.0f) {
 				m_acceleDelayTime = 0.0f;
-																	//*!
+																	
 			}
+			//カウントダウン終了後の加速処理
 			if (m_acceleDelayTime == 0.0f) {
-				// 加速度の割合を計算
+				//加速度の割合を計算
+				// 
 				m_velocity += m_accele * g_gameTime->GetFrameDeltaTime();
-				m_accele *= 0.5f;
+				//加速度を半減
+				//ドリフトする回数増えるごと加速力減少してしまう理由かも
+				//原因ではない
+				m_accele *= 0.5f;		
+
+				//低速以下の場合、加速度をリセット
 				if (m_accele.Length() < 0.1f) {
 					m_accele = Vector3::Zero;
 				}
@@ -226,7 +456,7 @@ namespace nsPlayer
 #endif
 			}
 		}
-		// 速度を進行方向に向けてゆっくり曲げていく
+		//速度を進行方向に向けてゆっくり曲げていく
 		Vector3 targetVelocity;	//目標
 		float t = m_velocity.Length();
 		targetVelocity = m_forward * t;
@@ -234,25 +464,25 @@ namespace nsPlayer
 
 		
 		//重力の設定
-		if (m_position.y > 0.0f) {
-			m_velocity.y -= 15.0f * g_gameTime->GetFrameDeltaTime();
+		if (m_position.y > GROUND_LEVEL) {
+			m_velocity.y -= GRAVITY * g_gameTime->GetFrameDeltaTime();
 		}
 		else {
 			m_velocity.y = 0.0f;
-			m_position.y = 0.0f;
+			m_position.y = GROUND_LEVEL;
 		}
 
-		if (m_velocity.Length() > 0.1f) {
-			//キャラクターコントローラーを使用して座標を更新
+		//キャラクターコントローラーを使用して座標を更新
+		if (m_velocity.Length() > STOP_THRESHOLD) {
 			m_position = m_charaCon.Execute(m_velocity, g_gameTime->GetFrameDeltaTime());
-			m_position.y += m_velocity.y * g_gameTime->GetFrameDeltaTime();
+			//m_position.y += m_velocity.y * g_gameTime->GetFrameDeltaTime();
 		}
 		else {
 			m_velocity = Vector3::Zero;
 		}
 		
 		
-		//座標を設定
+		//モデルの座標を設定
 		m_playerModel.SetPosition(m_position);
 		m_boardModel.SetPosition(m_position);
 		m_charaCon.SetPosition(m_position);
@@ -260,21 +490,24 @@ namespace nsPlayer
 
 	void Player::MoveLStickOn()
 	{
-		//ドリフト時の回転と移動
+		//左スティックとRBボタンでドリフト開始
 		if (g_pad[0]->IsPress(enButtonRB1) && (m_stickL.x != 0.0f || m_stickL.y != 0.0f)) {
 			m_isDrifting = true;
-			m_driftTime = 1.5f;
+			//ドリフト時間の設定
+			m_driftTime = 1.5f;		
 		}
 
 		//RBボタンが離されるか、ドリフト時間が終了
 		if (m_isDrifting && (!g_pad[0]->IsPress(enButtonB) || m_driftTime <= 0.0f)) {
 			m_isDrifting = false;
-			m_driftAngle = 0.0f; //角度をリセット
+			//ドリフト角度をリセット
+			m_driftAngle = 0.0f; 
 		}
 	}
 
 	void Player::Render(RenderContext& rc)
 	{
+		//プレイヤーモデルとボードモデルを描画
 		m_playerModel.Draw(rc);
 		m_boardModel.Draw(rc);
 	}
--- GameTemplate/Game/Player.h
+++ GameTemplate/Game/Player.h
@@ -1,10 +1,18 @@
 #pragma once
 
+class BackGround;
+
+namespace
+{
+	const float ADD_ACCELE = 10000.0f;		//追加加速度
+	const float JUMP_VALUE = 600.0f;	//ジャンプする数値
+}
 namespace nsPlayer
 {
 	//前方宣言
 	class IPlayerState;
 	class PlayerRunState;
+
 
 	class Player : public IGameObject
 	{
@@ -12,14 +20,14 @@ namespace nsPlayer
 		//アニメーションクリップ
 		enum EnAnimationClip
 		{
-			enAnimClip_Idle,
-			enAnimClip_Start,
-			enAnimClip_Push,
-			enAnimClip_Run,
-			enAnimClip_Jump,
-			enAnimClip_Drift,
+			enAnimClip_Idle,	//待機アニメーション
+			enAnimClip_Start,	//スタート時のアニメーション
+			enAnimClip_Push,	//プッシュ時のアニメーション
+			enAnimClip_Run,		//走行アニメーション
+			enAnimClip_Jump,	//ジャンプアニメーション
+			enAnimClip_Drift,	//ドリフトアニメーション
 			//enAnimClip_Angry,
-			enAnimClip_Num
+			enAnimClip_Num		//アニメーションクリップの数
 		};
 
 		////ステート関係
@@ -33,24 +41,31 @@ namespace nsPlayer
 		Player();
 		~Player();
 		bool Start();
-		void InitPlayerAnimationClips(); //プレイヤーのアニメーションクリップを初期化
+		void InitPlayerAnimationClips();
 		void Move();
 		void MoveLStickOn();
 		void Speed();
 		void Friction();
+		void Output();
+		void BackGroundCollisionCheck();
+		void CheckSpeedFromMovement();
+		void CheckCollisionWithWall();
+		void ApplySpeedLimit();
+		//void SetBackGround(BackGround* background);
 		//void Jump();
 		
 
 		void Update();
 		//void Update_NotPause();
 		void PlayAnimation(EnAnimationClip currentAnimtionClip);
+		void PlaySetAnimationSpeed(float m_animationSpeed);
 		//void OnAnimationEvent(const wchar_t* clipName, const wchar_t* eventName);//アニメーションイベント用の関数
 		void Render(RenderContext& rc);
 
 		/// <summary>
 		/// プレイヤーの前方方向を取得
 		/// </summary>
-		/// <returns></returns>
+		/// <returns><プレイヤーの前方方向ベクトル/returns>
 		const Vector3& GetForward() const
 		{
 			return m_forward;
@@ -58,7 +73,7 @@ namespace nsPlayer
 		/// <summary>
 		/// 座標の設定
 		/// </summary>
-		/// <param name="pos"></param>
+		/// <param name="pos">設定する座標</param>
 		void SetPosition(const Vector3 pos)
 		{
 			m_position = pos;
@@ -67,7 +82,7 @@ namespace nsPlayer
 		/// <summary>
 		/// 大きさの設定
 		/// </summary>
-		/// <param name="scale"></param>
+		/// <param name="scale">設定するスケール</param>
 		void SetScale(const Vector3 scale)
 		{
 			m_scale = scale;
@@ -76,7 +91,7 @@ namespace nsPlayer
 		/// <summary>
 		/// 回転の設定
 		/// </summary>
-		/// <param name="rot"></param>
+		/// <param name="rot">設定する回転量</param>
 		void SetRotation(const Quaternion& rot)
 		{
 			m_rotation = rot;
@@ -96,7 +111,7 @@ namespace nsPlayer
 		/// <summary>
 		/// 座標の取得
 		/// </summary>
-		/// <returns></returns>
+		/// <returns>プレイヤーの現在の座標</returns>
 		const Vector3& GetPostion() const
 		{
 			return m_position;
@@ -105,7 +120,7 @@ namespace nsPlayer
 		/// <summary>
 		/// 回転量の取得
 		/// </summary>
-		/// <returns></returns>
+		/// <returns>プレイヤーの現在の回転量</returns>
 		const Quaternion& GetRotation() const
 		{
 			return m_rotation;
@@ -114,7 +129,7 @@ namespace nsPlayer
 		/// <summary>
 		/// プレイヤーの速さの取得
 		/// </summary>
-		/// <returns></returns>
+		/// <returns>プレイヤーの速度ベクトル</returns>
 		const Vector3& GetPlayerSpped() const
 		{
 			return m_moveSpeed;
@@ -125,7 +140,7 @@ namespace nsPlayer
 		/// <summary>
 		/// アニメーション再生中かどうかの取得
 		/// </summary>
-		/// <returns></returns>
+		/// <returns>アニメーション再生中であるかを示すbool値</returns>
 		const bool& GetIsPlayingAnimation() const
 		{
 			return m_playerModel.IsPlayingAnimation();
@@ -137,18 +152,25 @@ namespace nsPlayer
 		void SetJump()
 		{
 			if (m_velocity.y == 0.0f) {
-				m_velocity.y = 600.0f;
+				m_velocity.y = JUMP_VALUE;
 			}
 		}
 
+		/// <summary>
+		/// ブレーキをかけて速度をゼロにする
+		/// </summary>
 		void SetBrake()
 		{
-			m_velocity.x = 0.0f;
-			m_velocity.z = 0.0f;
+			m_velocity.x = 0.00f;
+			m_velocity.z = 0.00f;
 			//m_velocity.x *= 0.999f * g_gameTime->GetFrameDeltaTime();
 			//m_velocity.z *= 0.999f * g_gameTime->GetFrameDeltaTime();
 		}
 
+		/// <summary>
+		/// プレイヤーの速度を取得
+		/// </summary>
+		/// <returns>現在の速度ベクトル</returns>
 		const Vector3& GetPlayerVelocity() const
 		{
 			return m_velocity;
@@ -157,9 +179,15 @@ namespace nsPlayer
 		{
 			m_player.GetRotation().y += 90.0f;
 		}*/
+		
+		/// <summary>
+		/// 初期減速度を設定
+		/// </summary>
+		/// <returns>初期減速度</returns>
 		const bool InitQuietTimeSet()
 		{
-			initQuietSeppd = addAccele / initQuietTime;
+			//加速度を初期静止時間で割り、減速度を算出
+			initQuietSeppd = ADD_ACCELE / initQuietTime;
 			return initQuietSeppd;
 		}
 
@@ -181,10 +209,11 @@ namespace nsPlayer
 		{
 			this->m_acceleDelayTime = delayTime;
 			this->m_accele = accele;
+			ApplySpeedLimit();
 		}
 
 		/// <summary>
-		/// 
+		/// ドリフト時間を設定
 		/// </summary>
 		/// <param name="driftTime"></param>
 		/// <returns></returns>
@@ -193,87 +222,133 @@ namespace nsPlayer
 			this->m_driftTime = driftTime;
 		}
 
+		/// <summary>
+		/// アクセルスタートしているかの取得
+		/// </summary>
+		/// <returns></returns>
 		const bool GetIsAcceleStart()
 		{
 			return m_isAcceleStart;
 		}
 
+		/// <summary>
+		/// アクセル開始しているかの設定
+		/// </summary>
+		/// <param name="acceleStart"></param>
 		void SetIsAcceleStart(const bool acceleStart)
 		{
 			m_isAcceleStart = acceleStart;
 		}
 
-		const void A(Vector3& speed, float& quietTime)	//*!
-		{
-			Vector3 subSpeed;	//減算
-			subSpeed = speed / quietTime;
-			m_velocity -= subSpeed;
+		//const void A(Vector3& speed, float& quietTime)	
+		//{
+		//	Vector3 subSpeed;	//減算
+		//	subSpeed = speed / quietTime;
+		//	m_velocity -= subSpeed;
 
-			if (m_velocity.x < 0.0f) {
-				m_velocity.x = 0.0f;
-			}
-			if (m_velocity.z < 0.0f) {
-				m_velocity.z = 0.0f;
-			}
-		}												//*!
-	
+		//	if (m_velocity.x < 0.0f) {
+		//		m_velocity.x = 0.0f;
+		//	}
+		//	if (m_velocity.z < 0.0f) {
+		//		m_velocity.z = 0.0f;
+		//	}
+		//}	
+
+		/// <summary>
+		/// ドリフト開始しているかの設定
+		/// </summary>
+		/// <param name="driftStart"></param>
 		void SetIsDriftStart(bool driftStart)
 		{
 			m_isDrifting = driftStart;
 		}
 
+		/// <summary>
+		/// ドリフト時間の取得
+		/// </summary>
+		/// <returns></returns>
 		float GetDriftTime()
 		{
 			return m_driftTime;
 		}
 
-	private:
-		ModelRender m_playerModel; //プレイヤーモデル
-		ModelRender m_boardModel;  //ボードモデル
-		Vector3 m_forward = Vector3::Front;		// プレイヤーの前方方向
-		Vector3 m_position = Vector3::Zero;		  //座標
-		Vector3 m_moveSpeed = Vector3::Zero;	//移動速度
-		Vector3 m_scale = Vector3::One;			//スケール
-		CharacterController m_charaCon;			//キャラクターコントローラー
-		Quaternion m_rotation;					//クォータニオン
-		Vector3 m_stickL = Vector3::Zero;		//Lスティック入力量
-		float m_complementTime = 0.0f;			//アニメーションの補間時間
-		EnAnimationClip m_currentAnimationClip = enAnimClip_Idle; //現在設定されているアニメーションクリップ
-		AnimationClip m_playerAnimClips[enAnimClip_Num]; //プレイヤーのアニメーションクリップ
-		AnimationClip m_boardAnimClips[enAnimClip_Num];//ボードのアニメーションクリップ
-		Animation m_animation;					//アニメーションのインスタンス
-		//State m_state = enState_Idle;
-		IPlayerState* m_playerState = nullptr;
-		Vector3 m_accele = Vector3::Zero;		//加速度
-		//float m_accele = 0.0f;
-		Vector3 m_velocity = Vector3::Zero;		//速度
-		Vector3 m_friction = Vector3(0.1f, 0.1f, 0.1f);		//摩擦力
-		Vector3 frictionForce = Vector3::Zero;
-		const float maxAcceleration = 500.0f; //最大加速度
-		const float maxDeceleration = 0.2f;		//最大減衰率
-		Quaternion tsts = Quaternion::Identity;
+		/// <summary>
+		/// アクセル時間の設定
+		/// </summary>
+		/// <param name="acceleTime"></param>
+		void SetAcceleTime(float acceleTime)
+		{
+			this->m_acceleTime = acceleTime;
+		}
 
-		float addAccele = 10000.0f;				//追加加速度
-		float raitoAccele = 0.0f;				//加速割合
-		float initQuietTime = 5.0f;				//初期の静止時間(加速度1000.0fの時)
-		float initQuietSeppd = 0.0f;			//初期の減速度
+		/// <summary>
+		/// アクセル時間の取得
+		/// </summary>
+		/// <returns></returns>
+		const float GetAcceleTime()const
+		{
+			return m_acceleTime;
+		}
+
+		
+	private:
+		ModelRender			m_playerModel;								//プレイヤーモデル
+		ModelRender			m_boardModel;								//ボードモデル
+		Vector3				m_forward = Vector3::Front;					//プレイヤーの前方方向
+		Vector3				m_position = Vector3(1000.0f,10.0f,-220.0f);		//プレイヤーの現在座標
+		Vector3				m_moveSpeed = Vector3::Zero;				//プレイヤーの移動速度
+		Vector3				m_scale = Vector3::One;						//プレイヤーのスケール
+		CharacterController m_charaCon;									//キャラクターコントローラー
+		Quaternion			m_rotation;									//プレイヤーの回転量
+		Vector3				m_stickL = Vector3::Zero;					//Lスティック入力量
+		float				m_complementTime = 0.0f;					//アニメーションの補間時間
+		float				m_animationSpeed = 2.0f;					//アニメーション速度
+		EnAnimationClip		m_currentAnimationClip = enAnimClip_Idle;	//現在設定されているアニメーションクリップ
+		AnimationClip		m_playerAnimClips[enAnimClip_Num];			//プレイヤーのアニメーションクリップ
+		AnimationClip		m_boardAnimClips[enAnimClip_Num];			//ボードのアニメーションクリップ
+		Animation			m_animation;								//アニメーション管理のインスタンス
+		//State				m_state = enState_Idle;
+		IPlayerState*		m_playerState = nullptr;					//現在のプレイヤーステート
+		Vector3				m_accele = Vector3::Zero;					//加速度ベクトル
+		//float				m_accele = 0.0f;
+		Vector3				m_velocity = Vector3::Zero;					//現在の速度
+		Vector3				m_friction = Vector3(0.1f, 0.1f, 0.1f);		//摩擦力
+		Vector3				nextPosition = Vector3::Zero;				//次フレームの座標
+		Vector3				movementVector = Vector3::Zero;				//現在の座標と次のフレームの座標の移動ベクトル
+		//const float maxAcceleration = 500.0f;							//最大加速度
+		//const float maxDeceleration = 0.2f;							//最大減衰率
+		Quaternion			tsts = Quaternion::Identity;
+
+		
+		float				raitoAccele = 0.0f;							//加速割合
+		float				initQuietTime = 5.0f;						//初期の静止時間(加速度1000.0fの時)
+		float				initQuietSeppd = 0.0f;						//初期の減速度
 
 		//bool isBButtonPressed = g_pad[0]->IsPress(enButtonB);
 		//float m_acceleTimer = 0.0f; //タイマー変数
 		//bool m_isAcceleRequested = false; //Bボタンが押されたかどうか
 
-		float m_acceleDelayTime = 0.0f;		//タイマー変数
-		float m_driftTime = 1.15f;		//ドリフトタイマー変数
+		float				m_acceleDelayTime = 0.0f;					//タイマー変数
+		float				m_driftTime = 1.15f;						//ドリフトタイマー変数
+		float				m_acceleTime = 1.15f;						//アクセルタイマー変数
 
-		bool m_isDrifting = false;	//ドリフト中かどうか
-		float m_driftAngle = 0.0f;	//ドリフト中の角度
-		float m_driftRotaionSpeed = 2.0f; //ドリフト中の１フレームあたりの回転速度
-		float m_driftFriction = 0.8f;	//ドリフト中の摩擦係数
+		bool				m_isDrifting = false;						//ドリフト中かどうか
+		float				m_driftAngle = 0.0f;						//ドリフト中の角度
+		float				m_driftRotaionSpeed = 2.0f;					//ドリフト中の１フレームあたりの回転速度
+		float				m_driftFriction = 0.8f;						//ドリフト中の摩擦係数
 
-		float SpeedLimit = 800.0f;	//速度上限 *!
+		float				SpeedLimit = 800.0f;						//速度上限
 
-		bool m_isAcceleStart = false;	//アクセル開始するかどうか
+		bool				m_isAcceleStart = false;					//アクセル開始するかどうか
 		
-		float RotSpeed = -0.01f;
+		float				RotSpeed = -0.01f;							//回転速度
+		float				currentSpeed = 0.0f;						//現在の速度
+		float				movementLength = 0.0f;						//移動ベクトルの長さ
+		Vector3				m_reflection= Vector3::Zero;
+
+		BackGround*			m_backGround = nullptr;
+
+	protected:
+		CCapsuleCollider	m_capsuleCollider;							//遮蔽物確認用のコライダー
 	};
 }
