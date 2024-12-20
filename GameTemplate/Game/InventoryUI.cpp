f47c29474c9c6d459f685b65d7ec96d297a25343
SatakeIsao <ca01234009@kawahara.ac.jp>
2024-12-20 12:06:37 +0900

    InventoryUIクラスの更新（ゲームスタート時にUIを表示させる処理を追加）

--- GameTemplate/Game/InventoryUI.cpp
+++ GameTemplate/Game/InventoryUI.cpp
@@ -24,7 +24,8 @@ bool InventoryUI::Start()
 	//テスト用ハンバーガーグレースプライトの初期化
 	m_humburgerGrayAll.Init("Assets/modelData/hamburger2_gray2.DDS", 50.0f, 50.0f);
 	m_humburgerGrayAll.SetPosition(m_humburgerSp.GetPosition());
-	m_humburgerGrayAll.SetScale(2.0f);
+	m_humburgerGrayAll.SetScale(m_scale);
+	m_humburgerGrayAll.SetMulColor({ 1.0f, 1.0f, 1.0f, 1.0f });
 	m_humburgerGrayAll.Update();
 	//テスト用ピザスプライトの初期化
 	m_pizzaSp.Init("Assets/modelData/pizza2.DDS", 50.0f, 50.0f);
@@ -39,7 +40,8 @@ bool InventoryUI::Start()
 	//テスト用ピザグレースプライトの初期化
 	m_pizzaGrayAll.Init("Assets/modelData/pizza2_gray2.DDS", 50.0f, 50.0f);
 	m_pizzaGrayAll.SetPosition(m_pizzaSp.GetPosition());
-	m_pizzaGrayAll.SetScale(2.0);
+	m_pizzaGrayAll.SetScale(m_scale);
+	m_pizzaGrayAll.SetMulColor({ 1.0f, 1.0f, 1.0f, 1.0f });
 	m_pizzaGrayAll.Update();
 	//テスト用寿司スプライトの初期化
 	m_sushiSp.Init("Assets/modelData/sushi2.DDS", 50.0f, 50.0f);
@@ -54,7 +56,8 @@ bool InventoryUI::Start()
 	//テスト用寿司グレースプライトの初期化
 	m_sushiGrayAll.Init("Assets/modelData/sushi2_gray2.DDS", 50.0f, 50.0f);
 	m_sushiGrayAll.SetPosition(m_sushiSp.GetPosition());
-	m_sushiGrayAll.SetScale(2.0f);
+	m_sushiGrayAll.SetScale(m_scale);
+	m_sushiGrayAll.SetMulColor({ 1.0f, 1.0f, 1.0f, 1.0f });
 	m_sushiGrayAll.Update();
 	return true;
 }
@@ -62,7 +65,16 @@ bool InventoryUI::Start()
 void InventoryUI::Update()
 {
 	//スプライトを表示・非表示テストするための簡易的なボタン操作
-	ButtonTest();
+	//ButtonTest();
+
+	
+	if (g_pad[0]->IsTrigger(enButtonB) && m_scaleState == ITEM_SCALE_ZERO) 
+	{
+		m_scaleState = ITEM_SCALE_LARGE;
+		//m_scaleTimer = 0.0f;
+	}
+
+	SpriteScale();
 
 	//ハンバーガースプライトの更新
 	m_humburgerSp.Update();
@@ -80,10 +92,55 @@ void InventoryUI::Update()
 	//寿司グレースプライトの更新
 	m_sushiGrayHalf.Update();
 	m_sushiGrayAll.Update();
+
+	//各アイテムの状態に基づいてフラグを更新
+	m_isHasHamburger = (m_hamburgerState != ITEM_GRAY_ALL);
+	m_isHasPizza = (m_pizzaState != ITEM_GRAY_ALL);
+	m_isHasSushi = (m_sushiState != ITEM_GRAY_ALL);
 }
 
-void InventoryUI::SpriteSet()
+void InventoryUI::SpriteScale()
 {
+	//float deltaTime = g_gameTime->GetFrameDeltaTime();
+	//m_scaleTimer += deltaTime;
+
+	switch (m_scaleState) {
+	case ITEM_SCALE_ZERO:
+		m_scale = 0.0f;
+		m_humburgerGrayAll.SetScale(m_scale);
+		m_pizzaGrayAll.SetScale(m_scale);
+		m_sushiGrayAll.SetScale(m_scale);
+		break;
+	case ITEM_SCALE_LARGE:
+		m_scale += 0.125f;
+		if (m_scale >=  3.0f) {
+			m_scale = 3.0f;
+			m_scaleState = ITEM_SCALE_NORMAL;
+		}
+		m_humburgerGrayAll.SetScale(m_scale);
+		m_pizzaGrayAll.SetScale(m_scale);
+		m_sushiGrayAll.SetScale(m_scale);
+		
+		break;
+	case ITEM_SCALE_NORMAL:
+		m_scale -= 0.025f;
+		if (m_scale <= 2.0f) {
+			m_scale = 2.0f;
+			m_scaleState = ITEM_SCALE_FINAL;
+		}
+		m_humburgerGrayAll.SetScale(m_scale);
+		m_pizzaGrayAll.SetScale(m_scale);
+		m_sushiGrayAll.SetScale(m_scale);
+		break;
+	case ITEM_SCALE_FINAL:
+		m_scale = 2.0f;
+		m_humburgerGrayAll.SetScale(m_scale);
+		m_pizzaGrayAll.SetScale(m_scale);
+		m_sushiGrayAll.SetScale(m_scale);
+		break;
+	default:
+		break;
+	}
 }
 
 void InventoryUI::ButtonTest()
@@ -102,22 +159,101 @@ void InventoryUI::ButtonTest()
 	}
 }
 
+void InventoryUI::CalcAlphaAndScale(float& alpha, float& scale)
+{
+	//α値を大きくする
+	alpha += 0.075f;
+	if (alpha >= 1.0f)
+	{
+		alpha = 1.0f;
+	}
+
+	//大きさを大きくする
+	scale += 0.1f;
+	if (scale <= 1.0f)
+	{
+		scale = 1.0f;
+	}
+}
+
 void InventoryUI::NextHamburgerState()
 {
 	switch (m_hamburgerState) {
 		case ITEM_ALL:
-			m_hamburgerState = ITEM_GRAY_HALF;
+			//m_hamburgerState = ITEM_GRAY_ALL;
 			break;
 		case ITEM_GRAY_HALF:
-			m_hamburgerState = ITEM_GRAY_ALL;
+			m_hamburgerState = ITEM_ALL;
 			break;
 		case ITEM_GRAY_ALL:
-			m_hamburgerState = ITEM_ALL;
+			m_hamburgerState = ITEM_GRAY_HALF;
 			break;
 	}
 }
 
 void InventoryUI::NextPizzaState()
+{
+	switch (m_pizzaState) {
+	case ITEM_ALL:
+		//m_pizzaState = ITEM_GRAY_ALL;
+		break;
+	case ITEM_GRAY_HALF:
+		m_pizzaState = ITEM_ALL;
+		break;
+	case ITEM_GRAY_ALL:
+		m_pizzaState = ITEM_GRAY_HALF;
+		break;
+	}
+}
+
+void InventoryUI::NextSushiState()
+{
+	switch (m_sushiState) {
+	case ITEM_ALL:
+		//m_sushiState = ITEM_GRAY_ALL;
+		break;
+	case ITEM_GRAY_HALF:
+		m_sushiState = ITEM_ALL;
+		break;
+	case ITEM_GRAY_ALL:
+		m_sushiState = ITEM_GRAY_HALF;
+		break;
+	}
+}
+
+void InventoryUI::NextScaleState()
+{
+	switch (m_scaleState) {
+	case ITEM_SCALE_ZERO:
+		m_scaleState = ITEM_SCALE_LARGE;
+		break;
+	case ITEM_SCALE_LARGE:
+		m_scaleState = ITEM_SCALE_NORMAL;
+		break;
+	case ITEM_SCALE_NORMAL:
+		m_scaleState = ITEM_SCALE_FINAL;
+		break;
+	default:
+		break;
+	}
+}
+
+void InventoryUI::PreviousHamburgerState()
+{
+	switch (m_hamburgerState) {
+	case ITEM_ALL:
+		m_hamburgerState = ITEM_GRAY_HALF;
+		break;
+	case ITEM_GRAY_HALF:
+		m_hamburgerState = ITEM_GRAY_ALL;
+		break;
+	case ITEM_GRAY_ALL:
+		//m_hamburgerState = ITEM_ALL;
+		break;
+	}
+}
+
+void InventoryUI::PreviousPizzaState()
 {
 	switch (m_pizzaState) {
 	case ITEM_ALL:
@@ -127,12 +263,12 @@ void InventoryUI::NextPizzaState()
 		m_pizzaState = ITEM_GRAY_ALL;
 		break;
 	case ITEM_GRAY_ALL:
-		m_pizzaState = ITEM_ALL;
+		//m_pizzaState = ITEM_ALL;
 		break;
 	}
 }
 
-void InventoryUI::NextSushiState()
+void InventoryUI::PreviousSushiState()
 {
 	switch (m_sushiState) {
 	case ITEM_ALL:
@@ -142,7 +278,7 @@ void InventoryUI::NextSushiState()
 		m_sushiState = ITEM_GRAY_ALL;
 		break;
 	case ITEM_GRAY_ALL:
-		m_sushiState = ITEM_ALL;
+		//m_sushiState = ITEM_ALL;
 		break;
 	}
 }
--- GameTemplate/Game/InventoryUI.h
+++ GameTemplate/Game/InventoryUI.h
@@ -1,5 +1,8 @@
 #pragma once
 
+//namespace nsCustomerMan {
+//	class InventoryUI;
+//}
 class InventoryUI : public IGameObject
 {
 public:
@@ -9,16 +12,28 @@ public:
 		ITEM_GRAY_ALL
 	};
 
+	enum ItemScaleState {
+		ITEM_SCALE_ZERO,
+		ITEM_SCALE_LARGE,
+		ITEM_SCALE_NORMAL,
+		ITEM_SCALE_FINAL,
+	};
+
 	InventoryUI();
 	~InventoryUI();
 
 	bool Start();
 	void Update();
-	void SpriteSet();
+	void SpriteScale();
 	void ButtonTest();
+	void CalcAlphaAndScale(float& alpha,float& scale);
 	void NextHamburgerState();
 	void NextPizzaState();
 	void NextSushiState();
+	void NextScaleState();
+	void PreviousHamburgerState();
+	void PreviousPizzaState();
+	void PreviousSushiState();
 	void Render(RenderContext& rc);
 
 	/// <summary>
@@ -29,6 +44,85 @@ public:
 		m_isShowHumberger = showGray;
 	}
 
+	/// <summary>
+	/// 衝突がしているか
+	/// </summary>
+	/// <param name="isHasCollided"></param>
+	void SetIsHasCollided(bool isHasCollided) {
+		m_isHasCollided = isHasCollided;
+	}
+
+	/// <summary>
+	/// 衝突がしているかどうかの取得
+	/// </summary>
+	/// <returns>衝突がしているかを示すbool値</returns>
+	const bool& GetIsHasCollided() const
+	{
+		return m_isHasCollided;
+	}
+
+	/// <summary>
+	/// 食べ物を所持しているかどうかの取得
+	/// </summary>
+	/// <returns></returns>
+	const bool& GetIsHasHamburger() const
+	{
+		return m_isHasHamburger;
+	}
+
+	/// <summary>
+	/// 食べ物を所持しているかどうかの取得
+	/// </summary>
+	/// <returns></returns>
+	const bool& GetIsHasPizza() const
+	{
+		return m_isHasPizza;
+	}
+
+	/// <summary>
+	/// 食べ物を所持しているかどうかの取得
+	/// </summary>
+	/// <returns></returns>
+	const bool& GetIsHasSushi() const
+	{
+		return m_isHasSushi;
+	}
+
+	void SetItemState(ItemState hamburgerState)
+	{
+		m_hamburgerState = hamburgerState;
+	}
+
+	bool GetIsHasHamburger()
+	{
+		return m_isHasHamburger;
+	}
+
+	void SetIsHasHamburger(bool isHasHamburger)
+	{
+		m_isHasHamburger = isHasHamburger;
+	}
+
+	bool GetIsHasPizza()
+	{
+		return m_isHasPizza;
+	}
+
+	void SetIsHasPizza(bool isHasPizza)
+	{
+		m_isHasPizza = isHasPizza;
+	}
+
+	bool GetIsHasSushi()
+	{
+		return m_isHasSushi;
+	}
+
+	void SetIsHasSushi(bool isHasSushi)
+	{
+		m_isHasSushi = isHasSushi;
+	}
+	
 private:
 	SpriteRender	m_humburgerSp;					//ハンバーガー：色あり
 	SpriteRender	m_humburgerGrayHalf;			//ハンバーガー：グレーと色あり
@@ -39,12 +133,21 @@ private:
 	SpriteRender	m_sushiSp;						//寿司：色あり
 	SpriteRender	m_sushiGrayHalf;				//寿司：グレーと色あり
 	SpriteRender	m_sushiGrayAll;					//寿司：グレー
+	float			m_scaleTimer = 0.0f;
+	float			m_scale = 0.0f;
 	bool			m_isShowHumberger = false;		//ハンバーガーの色あり画像を表示するかどうか
 	bool			m_isShowPizza = false;			//ピザの色あり画像を表示するかどうか
 	bool			m_isShowSushi = false;			//寿司の色あり画像を表示するかどうか
+	bool			m_isHasCollided = false;
+	bool			m_isHasHamburger = false;
+	bool			m_isHasPizza = false;
+	bool			m_isHasSushi = false;
 
-	ItemState		m_hamburgerState = ITEM_ALL;	//初期状態
-	ItemState		m_pizzaState = ITEM_ALL;		//初期状態
-	ItemState		m_sushiState = ITEM_ALL;		//初期状態
+	ItemState		m_hamburgerState = ITEM_GRAY_ALL;	//初期状態
+	ItemState		m_pizzaState = ITEM_GRAY_ALL;		//初期状態
+	ItemState		m_sushiState = ITEM_GRAY_ALL;		//初期状態
+	ItemScaleState  m_scaleState = ITEM_SCALE_ZERO;		//初期状態
+
+	//friend class nsCustomerMan::InventoryUI;
 };
 
