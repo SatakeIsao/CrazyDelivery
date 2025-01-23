7d4056dcdf49c3fc23fea96f43090a0ae11af18d
SatakeIsao <ca01234009@kawahara.ac.jp>
2025-01-23 17:45:40 +0900

    インベントリークラスを修正

--- GameTemplate/Game/InventoryUI.cpp
+++ GameTemplate/Game/InventoryUI.cpp
@@ -1,5 +1,30 @@
 #include "stdafx.h"
 #include "InventoryUI.h"
+#include "ShopHamburger.h"
+#include "ShopPizza.h"
+#include "ShopSushi.h"
+#include "CustomerMan.h"
+#include "CustomerMan_Hamburger.h"
+#include "CustomerMan_Pizza.h"
+#include "CustomerMan_Sushi.h"
+#include "GameSound.h"
+#include "ResultUI.h"
+#include "GameTimer.h"
+
+namespace
+{
+	Vector3 MONEY_PLANE_STARTPOS = Vector3(1920.0f, 100.0f, 0.0f);
+	Vector3 MONEY_PLANE_STOPPOS = Vector3(0.0f, 100.0f, 0.0f);
+	Vector3 MONEY_PLANE_ENDPOS = Vector3(-1920.0f, 100.0f, 0.0f);
+	
+
+	Vector3 HAMBURGER_LEFT_ENDPOS = Vector3(325, 270, 0);
+	Vector3 PIZZA_LEFT_ENDPOS = Vector3(507, 270, 0);
+	Vector3 SUSHI_LEFT_ENDPOS = Vector3(686, 270, 0);
+	float FOOD_SLIDESPEED = 1000.0f;
+	float MONEY_PLANE_SLIDESPEED = 4000.0f;
+	float REWARD_PLANE_STOPDURATION = 0.5f;
+}
 
 InventoryUI::InventoryUI()
 {
@@ -11,6 +36,22 @@ InventoryUI::~InventoryUI()
 
 bool InventoryUI::Start()
 {
+	m_resultUI = FindGO<ResultUI>("resultui");
+	m_gameTimer = FindGO<GameTimer>("gametimer");
+	m_shopHamburger = FindGOs<ShopHamburger>("shophamburger");
+	auto* m_shopHamburgerB = FindGO<ShopHamburger>("shophamburgerB");
+	m_shopPizza = FindGOs<ShopPizza>("shoppizza");
+	auto* m_shopPizzaB = FindGO<ShopPizza>("shoppizzaB");
+	m_shopSushi = FindGOs<ShopSushi>("shopsushi");
+	auto* m_shopSushiB = FindGO<ShopSushi>("shopsushiB");
+
+	m_customerMan_Hamburger = FindGOs<CustomerMan_Hamburger>("customerman_hamburger");
+	auto* m_customerMan_HamburgerB = FindGO<CustomerMan_Hamburger>("customerman_hamburgerB");
+	m_customerMan_Pizza = FindGOs<CustomerMan_Pizza>("customerman_pizza");
+	auto* m_customerMan_PizzaB = FindGO<CustomerMan_Pizza>("customerman_pizzaB");
+	m_customerMan_Sushi = FindGOs<CustomerMan_Sushi>("customerman_sushi");
+	auto* m_customerMan_SushiB = FindGO<CustomerMan_Sushi>("customerMan_sushiB");
+
 	//テスト用ハンバーガースプライトの初期化
 	m_humburgerSp.Init("Assets/modelData/hamburger2.DDS", 50.0f, 50.0f);
 	m_humburgerSp.SetPosition(Vector3(400.0f, 270.0f, 0.0f));
@@ -59,19 +100,221 @@ bool InventoryUI::Start()
 	m_sushiGrayAll.SetScale(m_scale);
 	m_sushiGrayAll.SetMulColor({ 1.0f, 1.0f, 1.0f, 1.0f });
 	m_sushiGrayAll.Update();
+	//IGotの板
+	m_gotPlane.sprite.Init("Assets/skaterData/PlayerUI_Get3.dds", 1920.0f, 1080.0f);
+	m_gotPlane.position = MONEY_PLANE_STARTPOS;
+	m_gotPlane.sprite.SetPosition(m_gotPlane.position);
+	m_gotPlane.sprite.SetScale(1.0f);
+	m_gotPlane.sprite.SetMulColor({ 1.0f,1.0f,1.0f,1.0f });
+	m_gotPlane.sprite.Update();
+
+	//ハンバーガー
+	m_gotHamburger.foodSprite.Init("Assets/skaterData/hamburgerGet.dds", 1920.0f, 1080.0f);
+	m_gotHamburger.foodPos = MONEY_PLANE_STARTPOS;
+	m_gotHamburger.foodSprite.SetPosition(m_gotPlane.position);
+	m_gotHamburger.foodSprite.SetScale(1.0f);
+	m_gotHamburger.foodSprite.SetMulColor({ 1.0f,1.0f,1.0f,1.0f });
+	m_gotHamburger.foodSprite.Update();
+
+	//ピザ
+	m_gotPizza.foodSprite.Init("Assets/skaterData/pizzaGet.dds", 1920.0f, 1080.0f);
+	m_gotPizza.foodPos = MONEY_PLANE_STARTPOS;
+	m_gotPizza.foodSprite.SetPosition(m_gotPlane.position);
+	m_gotPizza.foodSprite.SetScale(1.0f);
+	m_gotPizza.foodSprite.SetMulColor({ 1.0f,1.0f,1.0f,1.0f });
+	m_gotPizza.foodSprite.Update();
+	
+	//寿司
+	m_gotSushi.foodSprite.Init("Assets/skaterData/sushiGet.dds", 1920.0f, 1080.0f);
+	m_gotSushi.foodPos = MONEY_PLANE_STARTPOS;
+	m_gotSushi.foodSprite.SetPosition(m_gotPlane.position);
+	m_gotSushi.foodSprite.SetScale(1.0f);
+	m_gotSushi.foodSprite.SetMulColor({ 1.0f,1.0f,1.0f,1.0f });
+	m_gotSushi.foodSprite.Update();
+
+	//報酬をもらった時の板
+	m_reward150.sprite.Init("Assets/skaterData/Reward150_2UI.dds", 1920.0f, 1080.0f);
+	m_reward150.sprite.SetPosition(MONEY_PLANE_STARTPOS);
+	m_reward150.sprite.SetScale(1.0f);
+	m_reward150.sprite.SetMulColor({ 1.0f,1.0f,1.0f,1.0f });
+	m_reward150.sprite.Update();
+	//報酬200
+	m_reward200.sprite.Init("Assets/skaterData/Reward200_2UI.dds", 1920.0f, 1080.0f);
+	m_reward200.sprite.SetPosition(MONEY_PLANE_STARTPOS);
+	m_reward200.sprite.SetScale(1.0f);
+	m_reward200.sprite.SetMulColor({ 1.0f,1.0f,1.0f,1.0f });
+	m_reward200.sprite.Update();
+	//報酬500
+	m_reward500.sprite.Init("Assets/skaterData/Reward500_2UI.dds", 1920.0f, 1080.0f);
+	m_reward500.sprite.SetPosition(MONEY_PLANE_STARTPOS);
+	m_reward500.sprite.SetScale(1.0f);
+	m_reward500.sprite.SetMulColor({ 1.0f,1.0f,1.0f,1.0f });
+	m_reward500.sprite.Update();
+
+	m_soldOut.sprite.Init("Assets/skaterData/SoldOutUI.dds", 1920.0f, 1080.0f);
+	m_soldOut.sprite.SetPosition(MONEY_PLANE_STARTPOS);
+	m_soldOut.sprite.SetScale(1.0f);
+	m_soldOut.sprite.SetMulColor({ 1.0f,1.0f,1.0f,1.0f });
+	m_soldOut.sprite.Update();
 	return true;
 }
 
 void InventoryUI::Update()
 {
-	//スプライトを表示・非表示テストするための簡易的なボタン操作
-	//ButtonTest();
+
+	//お客さんの衝突判定とスプライト設定
+	for (int i = 0; i < m_customerMan_Hamburger.size(); i++)
+	{
+		CustomerMan_Hamburger* customerHamburger = m_customerMan_Hamburger[i];
+		if (customerHamburger->GetIsHasCollided_Man()
+			&& customerHamburger->GetScoreResetTimer() >= 3.0f)
+		{
+			//重複設定を防ぐ
+			if (m_currentRewardSprite != &m_reward150)
+			{
+				SetRewardSprite(&m_reward150);
+			}
+		}
+	}
+
+	//お客さんの衝突判定とスプライト設定
+	for (int i = 0; i < m_customerMan_Pizza.size(); i++)
+	{
+		CustomerMan_Pizza* customerPizza = m_customerMan_Pizza[i];
+		if (customerPizza->GetIsHasCollided_Man()
+			&& customerPizza->GetScoreResetTimer() >= 3.0f)
+		{
+			//重複設定を防ぐ
+			if (m_currentRewardSprite != &m_reward200)
+			{
+				SetRewardSprite(&m_reward200);
+			}
+		}
+	}
+
+	//お客さんの衝突判定とスプライト設定
+	for (int i = 0; i < m_customerMan_Sushi.size(); i++)
+	{
+		CustomerMan_Sushi* customerSushi = m_customerMan_Sushi[i];
+		if (customerSushi->GetIsHasCollided_Man()
+			&& customerSushi->GetScoreResetTimer() >= 3.0f)
+		{
+			//重複設定を防ぐ
+			if (m_currentRewardSprite != &m_reward500)
+			{
+				SetRewardSprite(&m_reward500);
+			}
+		}
+	}
+
+	// gotPlane の描画処理
+	for (int i = 0; i < m_shopHamburger.size(); i++)
+	{
+		ShopHamburger* shop = m_shopHamburger[i];
+		if (shop->GetIsHamburgerUIMove()
+			&& shop->GetCoolDownTimer() >= 7.0f)
+		{
+			if (!shop->GetIsHamburgerFull())
+			{
+				SetRewardSprite(&m_gotPlane);
+				SetFoodSprite(&m_gotHamburger);
+
+				m_foodGotSE = NewGO<SoundSource>(0);
+				m_foodGotSE->Init(enSoundName_foodGot);
+				m_foodGotSE->SetVolume(1.0f);
+				m_foodGotSE->Play(false);
+			}
+			else if (shop->GetIsHamburgerFull())
+			{
+				m_soldOutSE = NewGO <SoundSource>(0);
+				m_soldOutSE->Init(enSoundName_SoldOut);
+				m_soldOutSE->SetVolume(1.0f);
+				m_soldOutSE->Play(false);
+
+				SetRewardSprite(&m_soldOut);
+			}
+		}
+	}
+	
+	for (int i = 0; i < m_shopPizza.size(); i++)
+	{
+		ShopPizza* shopPizza = m_shopPizza[i];
+		if (shopPizza->GetIsPizzaUIMove()
+			&& shopPizza->GetCoolDownTimer() >= 7.0f)
+		{
+			if(!shopPizza->GetIsPizzaFull())
+			{
+				SetRewardSprite(&m_gotPlane);
+				SetFoodSprite(&m_gotPizza);
+
+				m_foodGotSE = NewGO<SoundSource>(0);
+				m_foodGotSE->Init(enSoundName_foodGot);
+				m_foodGotSE->SetVolume(1.0f);
+				m_foodGotSE->Play(false);
+			}
+			else if (shopPizza->GetIsPizzaFull())
+			{
+				SetRewardSprite(&m_soldOut);
+
+				m_soldOutSE = NewGO <SoundSource>(0);
+				m_soldOutSE->Init(enSoundName_SoldOut);
+				m_soldOutSE->SetVolume(1.0f);
+				m_soldOutSE->Play(false);
+			}
+		}
+	}
+	for (int i = 0; i < m_shopSushi.size(); i++)
+	{
+		ShopSushi* shopSushi = m_shopSushi[i];
+		if (shopSushi->GetIsSushiUIMove()
+			&& shopSushi->GetCoolDownTimer() >= 7.0f)
+		{
+			if (!shopSushi->GetIsSushiFull())
+			{
+				SetRewardSprite(&m_gotPlane);
+				SetFoodSprite(&m_gotSushi);
+
+				m_foodGotSE = NewGO<SoundSource>(0);
+				m_foodGotSE->Init(enSoundName_foodGot);
+				m_foodGotSE->SetVolume(1.0f);
+				m_foodGotSE->Play(false);
+			}
+			
+			else if(shopSushi->GetIsSushiFull())
+			{
+				SetRewardSprite(&m_soldOut);
+
+				m_soldOutSE = NewGO <SoundSource>(0);
+				m_soldOutSE->Init(enSoundName_SoldOut);
+				m_soldOutSE->SetVolume(1.0f);
+				m_soldOutSE->Play(false);
+			}
+		}
+	}
+
+	//クールタイム終了時のスプライトリセット
+	if (m_currentRewardSprite != nullptr 
+		&& m_currentRewardSprite->state == Sliding_To_End
+		&& m_currentRewardSprite->position.x <= MONEY_PLANE_ENDPOS.x)
+	{
+		m_currentRewardSprite = nullptr;
+		m_isRewardSpriteInitialized = false;	//フラグをリセット
+	}
+	//スプライトのスライド処理
+	if (m_currentRewardSprite != nullptr)
+	{
+		SpriteSlide(*m_currentRewardSprite);
+	}
+
+	if (m_currentFoodSprite != nullptr)
+	{
+		SpriteSlideFood(*m_currentFoodSprite);
+	}
 
 	
 	if (g_pad[0]->IsTrigger(enButtonB) && m_scaleState == ITEM_SCALE_ZERO) 
 	{
 		m_scaleState = ITEM_SCALE_LARGE;
-		//m_scaleTimer = 0.0f;
 	}
 
 	SpriteScale();
@@ -101,9 +344,6 @@ void InventoryUI::Update()
 
 void InventoryUI::SpriteScale()
 {
-	//float deltaTime = g_gameTime->GetFrameDeltaTime();
-	//m_scaleTimer += deltaTime;
-
 	switch (m_scaleState) {
 	case ITEM_SCALE_ZERO:
 		m_scale = 0.0f;
@@ -143,6 +383,191 @@ void InventoryUI::SpriteScale()
 	}
 }
 
+void InventoryUI::SpriteSlide(RewardSprite& rewardSprite)
+{
+	float deltaTime = g_gameTime->GetFrameDeltaTime();
+
+	switch (rewardSprite.state){
+	case Sliding_To_Stop: {
+		//停止位置に向かって移動
+		rewardSprite.position.x -= MONEY_PLANE_SLIDESPEED * deltaTime;
+
+		if (rewardSprite.position.x <= MONEY_PLANE_STOPPOS.x)
+		{
+			rewardSprite.position.x = MONEY_PLANE_STOPPOS.x;
+			rewardSprite.state = Stopped;
+			rewardSprite.stopTimer = 0.0f;
+		}
+		break;
+	}
+	case Stopped:
+	{
+		//停止時間をカウント
+		rewardSprite.stopTimer += deltaTime;
+
+		if (rewardSprite.stopTimer >= REWARD_PLANE_STOPDURATION)
+		{
+			rewardSprite.state = Sliding_To_End;
+		}
+		break;
+	}
+	case Sliding_To_End:
+	{
+		//終了位置に向かって移動
+		rewardSprite.position.x -= MONEY_PLANE_SLIDESPEED * deltaTime;
+
+		if (rewardSprite.position.x <= MONEY_PLANE_ENDPOS.x)
+		{
+			rewardSprite.position.x = MONEY_PLANE_ENDPOS.x;
+			rewardSprite.state = Stopped;
+		}
+		break;
+	}
+	default:
+		break;
+	}
+	rewardSprite.sprite.SetPosition(rewardSprite.position);
+	rewardSprite.sprite.Update();
+}
+
+void InventoryUI::SpriteSlideFood(FoodSprite& foodSprite)
+{
+	float deltaTime = g_gameTime->GetFrameDeltaTime();
+
+	switch (foodSprite.foodState) {
+	case Sliding_To_Stop: {
+		//停止位置に向かって移動
+		foodSprite.foodPos.x -= MONEY_PLANE_SLIDESPEED * deltaTime;
+
+		if (foodSprite.foodPos.x <= MONEY_PLANE_STOPPOS.x)
+		{
+			foodSprite.foodPos.x = MONEY_PLANE_STOPPOS.x;
+			foodSprite.foodState = Stopped;
+			foodSprite.stopTimer = 0.0f;
+		}
+	}
+	break;
+	case Stopped:
+	{
+		//停止時間をカウント
+		foodSprite.stopTimer += deltaTime;
+		m_isNextOn = false;
+
+		if (foodSprite.stopTimer >= REWARD_PLANE_STOPDURATION)
+		{
+			if (m_shopHamburger[0]->GetIsHamburgerUIMove()
+				|| m_shopHamburger[1]->GetIsHamburgerUIMove())
+			{
+				foodSprite.foodState = Sliding_To_HamburgerLeftEnd;
+				foodSprite.foodScale = 1.0f;
+				foodSprite.stopTimer = 0.0f;
+			}
+			else if (m_shopPizza[0]->GetIsPizzaUIMove()
+					|| m_shopPizza[1]->GetIsPizzaUIMove())
+			{
+				foodSprite.foodState = Sliding_To_PizzaLeftEnd;
+				foodSprite.foodScale = 1.0f;
+				foodSprite.stopTimer = 0.0f;
+			}
+			else if (m_shopSushi[0]->GetIsSushiUIMove()
+				|| m_shopSushi[1]->GetIsSushiUIMove())
+			{
+				foodSprite.foodState = Sliding_To_SushiLeftEnd;
+				foodSprite.foodScale = 1.0f;
+				foodSprite.stopTimer = 0.0f;
+			}
+		}
+
+	}
+	break;
+	case Sliding_To_End:
+	{
+		//終了位置に向かって移動
+		foodSprite.foodPos.x -= MONEY_PLANE_SLIDESPEED * deltaTime;
+
+		if (foodSprite.foodPos.x <= MONEY_PLANE_ENDPOS.x)
+		{
+			foodSprite.foodPos.x = MONEY_PLANE_ENDPOS.x;
+			foodSprite.foodState = Stopped;
+		}
+		
+	}
+	break;
+	case Sliding_To_HamburgerLeftEnd:
+	{
+		targetScale = foodSprite.foodScale * 0.5;
+		foodSprite.foodScale += (targetScale - foodSprite.foodScale) * 0.1;
+
+		//目標位置への方向ベクトルを計算
+		dir = HAMBURGER_LEFT_ENDPOS - foodSprite.foodPos;
+		distance = dir.Length();
+
+		if (distance > 50.1f)
+		{
+			dir.Normalize();
+			foodSprite.foodPos += dir * FOOD_SLIDESPEED * deltaTime;
+			
+		}
+		else
+		{
+			
+			foodSprite.foodPos = MONEY_PLANE_STARTPOS;
+			foodSprite.foodScale = 1.0f;
+			foodSprite.foodState = Stopped;
+		}
+		break;
+	}
+
+	case Sliding_To_PizzaLeftEnd:
+		m_targetPizzaScale = foodSprite.foodScale * 0.5f;
+		foodSprite.foodScale += (m_targetPizzaScale - foodSprite.foodScale) * 0.1f;
+
+		//目標位置へのベクトルを計算
+		dirPizza = PIZZA_LEFT_ENDPOS - foodSprite.foodPos;
+		distancePizza = dirPizza.Length();
+
+		if (distancePizza > 50.1f)
+		{
+			dirPizza.Normalize();
+			foodSprite.foodPos += dirPizza * FOOD_SLIDESPEED * deltaTime;
+		}
+		else
+		{
+			foodSprite.foodPos = MONEY_PLANE_STARTPOS;
+			foodSprite.foodScale = 1.0f;
+			foodSprite.foodState = Stopped;
+		}
+		break;
+
+	case Sliding_To_SushiLeftEnd:
+		m_targetSushiScale = foodSprite.foodScale * 0.5f;
+		foodSprite.foodScale += (m_targetSushiScale - foodSprite.foodScale) * 0.1f;
+		//目標位置へのベクトルを計算
+		m_dirSushi = SUSHI_LEFT_ENDPOS - foodSprite.foodPos;
+		m_distanceSushi = m_dirSushi.Length();
+
+		if (m_distanceSushi > 50.1f)
+		{
+			m_dirSushi.Normalize();
+			foodSprite.foodPos += m_dirSushi * FOOD_SLIDESPEED * deltaTime;
+		}
+		else
+		{
+			foodSprite.foodPos = MONEY_PLANE_STARTPOS;
+			foodSprite.foodScale = 1.0f;
+			foodSprite.foodState = Stopped;
+		}
+		break;
+
+	default:
+		break;
+	}
+	foodSprite.foodSprite.SetPosition(foodSprite.foodPos);
+	foodSprite.foodSprite.SetScale(foodSprite.foodScale);
+	foodSprite.foodSprite.Update();
+}
+
+//一時的なテスト
 void InventoryUI::ButtonTest()
 {
 	if (g_pad[0]->IsTrigger(enButtonUp))
@@ -180,13 +605,14 @@ void InventoryUI::NextHamburgerState()
 {
 	switch (m_hamburgerState) {
 		case ITEM_ALL:
-			//m_hamburgerState = ITEM_GRAY_ALL;
 			break;
 		case ITEM_GRAY_HALF:
 			m_hamburgerState = ITEM_ALL;
+			m_isHasFullHamburger = true;
 			break;
 		case ITEM_GRAY_ALL:
 			m_hamburgerState = ITEM_GRAY_HALF;
+			m_isHasFullHamburger = false;
 			break;
 	}
 }
@@ -195,13 +621,14 @@ void InventoryUI::NextPizzaState()
 {
 	switch (m_pizzaState) {
 	case ITEM_ALL:
-		//m_pizzaState = ITEM_GRAY_ALL;
 		break;
 	case ITEM_GRAY_HALF:
 		m_pizzaState = ITEM_ALL;
+		m_isHasFullPizza = true;
 		break;
 	case ITEM_GRAY_ALL:
 		m_pizzaState = ITEM_GRAY_HALF;
+		m_isHasFullPizza = false;
 		break;
 	}
 }
@@ -210,13 +637,14 @@ void InventoryUI::NextSushiState()
 {
 	switch (m_sushiState) {
 	case ITEM_ALL:
-		//m_sushiState = ITEM_GRAY_ALL;
 		break;
 	case ITEM_GRAY_HALF:
 		m_sushiState = ITEM_ALL;
+		m_isHasFullSushi = true;
 		break;
 	case ITEM_GRAY_ALL:
 		m_sushiState = ITEM_GRAY_HALF;
+		m_isHasFullSushi = false;
 		break;
 	}
 }
@@ -243,12 +671,12 @@ void InventoryUI::PreviousHamburgerState()
 	switch (m_hamburgerState) {
 	case ITEM_ALL:
 		m_hamburgerState = ITEM_GRAY_HALF;
+		m_isHasFullHamburger = false;
 		break;
 	case ITEM_GRAY_HALF:
 		m_hamburgerState = ITEM_GRAY_ALL;
 		break;
 	case ITEM_GRAY_ALL:
-		//m_hamburgerState = ITEM_ALL;
 		break;
 	}
 }
@@ -258,12 +686,12 @@ void InventoryUI::PreviousPizzaState()
 	switch (m_pizzaState) {
 	case ITEM_ALL:
 		m_pizzaState = ITEM_GRAY_HALF;
+		m_isHasFullPizza = false;
 		break;
 	case ITEM_GRAY_HALF:
 		m_pizzaState = ITEM_GRAY_ALL;
 		break;
 	case ITEM_GRAY_ALL:
-		//m_pizzaState = ITEM_ALL;
 		break;
 	}
 }
@@ -273,18 +701,57 @@ void InventoryUI::PreviousSushiState()
 	switch (m_sushiState) {
 	case ITEM_ALL:
 		m_sushiState = ITEM_GRAY_HALF;
+		m_isHasFullSushi = false;
 		break;
 	case ITEM_GRAY_HALF:
 		m_sushiState = ITEM_GRAY_ALL;
 		break;
 	case ITEM_GRAY_ALL:
-		//m_sushiState = ITEM_ALL;
 		break;
 	}
 }
 
+void InventoryUI::SetRewardSprite(RewardSprite* rewardSprite)
+{
+	m_currentRewardSprite = rewardSprite;
+	m_currentRewardSprite->position = MONEY_PLANE_STARTPOS;
+	m_currentRewardSprite->state = Sliding_To_Stop;
+	//スプライトの即時更新と描画
+	m_currentRewardSprite->sprite.SetPosition(m_currentRewardSprite->position);
+	m_currentRewardSprite->sprite.Update();
+	RenderImmediate(m_currentRewardSprite->sprite);
+}
+
+void InventoryUI::SetFoodSprite(FoodSprite* foodSprite)
+{
+	m_currentFoodSprite = foodSprite;
+	m_currentFoodSprite->foodPos = MONEY_PLANE_STARTPOS;
+	m_currentFoodSprite->foodState = Sliding_To_Stop;
+	//スケールをリセット
+	m_currentFoodSprite->foodScale = 1.0f;
+	//スプライトの即時更新と描画
+	m_currentFoodSprite->foodSprite.SetPosition(m_currentFoodSprite->foodPos);
+	m_currentFoodSprite->foodSprite.SetScale(m_currentFoodSprite->foodScale);
+	m_currentFoodSprite->foodSprite.Update();
+	RenderImmediate(m_currentFoodSprite->foodSprite);
+}
+
+void InventoryUI::RenderImmediate(SpriteRender& sprite)
+{
+	RenderContext& rc = g_graphicsEngine->GetRenderContext();
+	sprite.Draw(rc);
+}
+
+
+
 void InventoryUI::Render(RenderContext& rc)
 {
+	if(m_gameTimer->GetIsTimerEnd())
+	//if (m_resultUI->GetIsEnd() == true)
+	{
+		return;
+	}
+
 	switch (m_hamburgerState) {
 		case ITEM_ALL:
 			m_humburgerSp.Draw(rc);
@@ -321,4 +788,22 @@ void InventoryUI::Render(RenderContext& rc)
 		break;
 	}
 	
+	if (m_currentRewardSprite != nullptr)
+	{
+		m_currentRewardSprite->sprite.Draw(rc);
+	}
+
+	// フードスプライトを描画
+	if (m_currentFoodSprite && m_currentFoodSprite->IsVisible()) {
+		m_currentFoodSprite->Draw(rc);
+	}
+}
+
+void InventoryUI::SetRewardSpriteToGetPlane()
+{
+	m_currentRewardSprite = &m_gotPlane;
+	m_currentRewardSprite->position = MONEY_PLANE_STARTPOS;
+	m_currentRewardSprite->state = Sliding_To_Stop;
+	m_currentRewardSprite->sprite.SetPosition(m_currentRewardSprite->position);
+	m_currentRewardSprite->sprite.Update();
 }
--- GameTemplate/Game/InventoryUI.h
+++ GameTemplate/Game/InventoryUI.h
@@ -1,8 +1,17 @@
 #pragma once
 
-//namespace nsCustomerMan {
-//	class InventoryUI;
-//}
+
+class ShopHamburger;
+class ShopPizza;
+class ShopSushi;
+
+class CustomerMan;
+class CustomerMan_Hamburger;
+class CustomerMan_Pizza;
+class CustomerMan_Sushi;
+class ResultUI;
+class GameTimer;
+
 class InventoryUI : public IGameObject
 {
 public:
@@ -19,12 +28,59 @@ public:
 		ITEM_SCALE_FINAL,
 	};
 
+	enum RewardPlaneState {
+		Sliding_To_Stop,
+		Stopped,
+		Sliding_To_End,
+		Sliding_To_HamburgerLeftEnd,
+		Sliding_To_HamburgerRightEnd,
+		Sliding_To_PizzaLeftEnd,
+		Sliding_To_SushiLeftEnd,
+	};
+
+private:
+	struct RewardSprite
+	{
+		SpriteRender sprite;
+		Vector3 position = Vector3::Zero;
+		float stopTimer = 0.0f;
+		RewardPlaneState state = Sliding_To_Stop;
+	};
+
+	struct FoodSprite
+	{
+	
+		void SetVisible(bool isVisible) { m_isVisible = isVisible; }
+		bool IsVisible() const { return m_isVisible; }
+		
+		void Draw(RenderContext& rc)
+		{
+			if (m_isVisible)
+			{
+				foodSprite.Draw(rc);
+			}
+		}
+		
+		SpriteRender foodSprite;
+		Vector3 foodPos = Vector3::Zero;
+		float foodScale = 1.0f;
+		float stopTimer = 0.0f;
+		RewardPlaneState foodState = Sliding_To_Stop;
+		bool m_isVisible = true; // 初期状態は可視
+	};
+
+
+public:
+	
 	InventoryUI();
 	~InventoryUI();
 
 	bool Start();
 	void Update();
 	void SpriteScale();
+	void SpriteSlide(RewardSprite& rewardSprite);
+	void SpriteSlideFood(FoodSprite& foodSprite);
+	void SetRewardSpriteToGetPlane();
 	void ButtonTest();
 	void CalcAlphaAndScale(float& alpha,float& scale);
 	void NextHamburgerState();
@@ -34,8 +90,11 @@ public:
 	void PreviousHamburgerState();
 	void PreviousPizzaState();
 	void PreviousSushiState();
+	void SetRewardSprite(RewardSprite* rewardSprite);
+	void SetFoodSprite(FoodSprite* foodSprite);
+	void RenderImmediate(SpriteRender& sprite);
 	void Render(RenderContext& rc);
-
+		
 	/// <summary>
 	/// 表示するスプライトを切り替える関数
 	/// </summary>
@@ -122,32 +181,103 @@ public:
 	{
 		m_isHasSushi = isHasSushi;
 	}
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <returns></returns>
+	bool& GetIsHasFullHamburger()
+	{
+		return m_isHasFullHamburger;
+	}
+	
+	bool GetIsHasFullPizza()
+	{
+		return m_isHasFullPizza;
+	}
+
+	bool GetIsHasFullSushi()
+	{
+		return m_isHasFullSushi;
+	}
+
+	bool& GetIsNextOn()
+	{
+		return m_isNextOn;
+	}
+
+	void ResetHamburgerEndFlag()
+	{
+		m_isNextOn = false;
+	}
+
+	
 	
 private:
-	SpriteRender	m_humburgerSp;					//ハンバーガー：色あり
-	SpriteRender	m_humburgerGrayHalf;			//ハンバーガー：グレーと色あり
-	SpriteRender	m_humburgerGrayAll;				//ハンバーガー：グレー
-	SpriteRender	m_pizzaSp;						//ピザ：色あり
-	SpriteRender	m_pizzaGrayHalf;				//ピザ：グレーと色あり
-	SpriteRender	m_pizzaGrayAll;					//ピザ：グレー
-	SpriteRender	m_sushiSp;						//寿司：色あり
-	SpriteRender	m_sushiGrayHalf;				//寿司：グレーと色あり
-	SpriteRender	m_sushiGrayAll;					//寿司：グレー
-	float			m_scaleTimer = 0.0f;
-	float			m_scale = 0.0f;
-	bool			m_isShowHumberger = false;		//ハンバーガーの色あり画像を表示するかどうか
-	bool			m_isShowPizza = false;			//ピザの色あり画像を表示するかどうか
-	bool			m_isShowSushi = false;			//寿司の色あり画像を表示するかどうか
-	bool			m_isHasCollided = false;
-	bool			m_isHasHamburger = false;
-	bool			m_isHasPizza = false;
-	bool			m_isHasSushi = false;
+	std::vector<ShopHamburger*> m_shopHamburger;
+	std::vector<ShopPizza*> m_shopPizza;
+	std::vector<ShopSushi*> m_shopSushi;
 
-	ItemState		m_hamburgerState = ITEM_GRAY_ALL;	//初期状態
-	ItemState		m_pizzaState = ITEM_GRAY_ALL;		//初期状態
-	ItemState		m_sushiState = ITEM_GRAY_ALL;		//初期状態
-	ItemScaleState  m_scaleState = ITEM_SCALE_ZERO;		//初期状態
+	CustomerMan* m_customerMan = nullptr;
+	std::vector<CustomerMan_Hamburger*> m_customerMan_Hamburger;
+	std::vector<CustomerMan_Pizza*> m_customerMan_Pizza;
+	std::vector<CustomerMan_Sushi*> m_customerMan_Sushi;
 
-	//friend class nsCustomerMan::InventoryUI;
+	SpriteRender		m_humburgerSp;					//ハンバーガー：色あり
+	SpriteRender		m_humburgerGrayHalf;			//ハンバーガー：グレーと色あり
+	SpriteRender		m_humburgerGrayAll;				//ハンバーガー：グレー
+	SpriteRender		m_pizzaSp;						//ピザ：色あり
+	SpriteRender		m_pizzaGrayHalf;				//ピザ：グレーと色あり
+	SpriteRender		m_pizzaGrayAll;					//ピザ：グレー
+	SpriteRender		m_sushiSp;						//寿司：色あり
+	SpriteRender		m_sushiGrayHalf;				//寿司：グレーと色あり
+	SpriteRender		m_sushiGrayAll;					//寿司：グレー			
+	RewardSprite		m_reward150;					//報酬をもらった時の板
+	RewardSprite		m_reward200;
+	RewardSprite		m_reward500;
+	RewardSprite		m_gotPlane;						//IGotの板
+	RewardSprite		m_soldOut;
+	RewardSprite*		m_currentRewardSprite = nullptr;
+	
+	FoodSprite			m_gotSushi;
+	FoodSprite			m_gotPizza;
+	FoodSprite			m_gotHamburger;
+	FoodSprite*			m_currentFoodSprite = nullptr;
+	float				m_scaleTimer = 0.0f;
+	float				m_scale = 0.0f;
+	float				m_rewardPlaneStopTimer = 0.0f;
+	float				targetScale = 0.0f;
+	float				distance = 0.0f;
+	float				m_targetPizzaScale = 0.0f;
+	float				distancePizza = 0.0f;
+	float				m_targetSushiScale = 0.0f;
+	float				m_distanceSushi = 0.0f;
+	Vector3				dirPizza = Vector3::Zero;
+	Vector3				m_dirSushi = Vector3::Zero;
+
+	Vector3				dir = Vector3::Zero;
+	bool				m_isShowHumberger = false;		//ハンバーガーの色あり画像を表示するかどうか
+	bool				m_isShowPizza = false;			//ピザの色あり画像を表示するかどうか
+	bool				m_isShowSushi = false;			//寿司の色あり画像を表示するかどうか
+	bool				m_isHasCollided = false;
+	bool				m_isHasHamburger = false;
+	bool				m_isHasPizza = false;
+	bool				m_isHasSushi = false;
+	bool				m_isHasFullHamburger = false;	//ハンバーガーをフル所持かどうか
+	bool				m_isHasFullPizza = false;		//ピザをフル所持かどうか
+	bool				m_isHasFullSushi = false;		//寿司をフル所持かどうか
+	bool				m_isSlidingMoneyPlane = false;
+	bool				m_isRewardSpriteInitialized = false;
+	bool				m_isNextOn = false;
+
+	ItemState			m_hamburgerState = ITEM_GRAY_ALL;		//初期状態
+	ItemState			m_pizzaState = ITEM_GRAY_ALL;			//初期状態
+	ItemState			m_sushiState = ITEM_GRAY_ALL;			//初期状態
+	ItemScaleState		m_scaleState = ITEM_SCALE_ZERO;			//初期状態
+	RewardPlaneState	m_rewardPlaneState = Sliding_To_Stop;	//初期状態
+
+	ResultUI*			m_resultUI = nullptr;
+	GameTimer*			m_gameTimer = nullptr;
+	SoundSource*		m_soldOutSE = nullptr;
+	SoundSource*		m_foodGotSE = nullptr;
 };
 
